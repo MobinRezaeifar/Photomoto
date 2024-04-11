63e9d36c4cad2ff554aaa57740cf0fde100f614b
MobinRezaeifar <mobinrezaeifar@gmail.com>
2024-04-11 17:56:18 +0330

    update

--- Backend node/models/StoryModel.js
+++ Backend node/models/StoryModel.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
-const StorySchema = new mongoose.Schema({
-  owner: {
-    type: String,
-    require: true,
+const StorySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: String,
+      require: true,
+    },
+    media: {
+      type: String,
+      require: true,
+    },
+    time: {
+      type: String,
+      require: true,
+    },
   },
-  media: {
-    type: String,
-    require: true,
-  },
-  time: {
-    type: String,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("story", StorySchema);
