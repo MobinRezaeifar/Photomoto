a571939439b506718a6609537fbcdb3684d576cc
MobinRezaeifar <mobinrezaeifar@gmail.com>
2024-05-10 12:14:39 +0330

    .

--- Backend .net/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Backend .net/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed086728de7af422457a1f620b67d17ef56f9246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c175edd1c3345ac9236e745edaaf9d7a3dde7062")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
