8229cab27fb25a2f4b0fd21929097d7c2af21d67
MobinRezaeifar <mobinrezaeifar@gmail.com>
2024-05-30 15:12:25 +0330

    update Mobile App Main Section

--- Mobile App/lib/Route/Main.dart
+++ Mobile App/lib/Route/Main.dart
@@ -1,34 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../Utils/token_provider.dart';
+import 'package:myapp/components/Global/Direct.dart';
+import '../Utils/token_provider.dart'; // اطمینان حاصل کنید که این فایل پرووایدر شما است
 
 class Main extends StatelessWidget {
   const Main({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-      color: Colors.black,
-    );
-
     return Scaffold(
       body: Center(
-        child: Consumer(builder: (context, ref, _) {
-          final authState = ref.watch(authProvider);
-          if (authState.isAuthenticated && authState.token != null) {
-            return Text(
-              'Welcome to Photomoto! Token: ${authState.token}',
-              style: textStyle,
+        child: Consumer(
+          builder: (context, ref, _) {
+            final clickedIcon = ref.watch(clickedIconProvider);
+            return Container(
+              child: Column(
+                children: [
+                  Expanded(
+                      child: SingleChildScrollView(
+                    child: ref.read(clickedIconProvider.notifier).state == 'me'
+                        ? Direct()
+                        : Text(
+                            "Empty",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                  )),
+                  Container(
+                    color: Color(0xFF002733),
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.account_circle, color: Colors.white),
+                          onPressed: () {
+                            ref.read(clickedIconProvider.notifier).state = 'me';
+                            print(ref.read(clickedIconProvider.notifier).state);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.connect_without_contact,
+                              color: Colors.white),
+                          onPressed: () {
+                            ref.read(clickedIconProvider.notifier).state =
+                                'connection';
+                            print(ref.read(clickedIconProvider.notifier).state);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.search, color: Colors.white),
+                          onPressed: () {
+                            ref.read(clickedIconProvider.notifier).state =
+                                'search';
+                            print(ref.read(clickedIconProvider.notifier).state);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.chat, color: Colors.white),
+                          onPressed: () {
+                            ref.read(clickedIconProvider.notifier).state =
+                                'chat';
+                            print(ref.read(clickedIconProvider.notifier).state);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.home, color: Colors.white),
+                          onPressed: () {
+                            ref.read(clickedIconProvider.notifier).state =
+                                'home';
+                            print(ref.read(clickedIconProvider.notifier).state);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             );
-          } else {
-            return Text(
-              'Welcome to Photomoto!',
-              style: textStyle,
-            );
-          }
-        }),
+          },
+        ),
       ),
     );
   }
---
+++ Mobile App/lib/components/Global/Direct.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Direct extends StatelessWidget {
+  const Direct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        "sldk;aslkd;Lorem Ipsum is simply Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more reLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumcently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumdummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum;",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
