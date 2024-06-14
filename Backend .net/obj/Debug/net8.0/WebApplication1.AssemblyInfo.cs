ca46434b5a1c2a6633c0243d196654b96edeaa64
MobinRezaeifar <mobinrezaeifar@gmail.com>
2024-06-14 22:00:09 +0330

    update Posts Api

--- Backend .net/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Backend .net/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4416acb5d8f0eb57f9cd777d354fe17cafe16306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4980a0de7be95cd5971c499e2212f403f2fd19")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
